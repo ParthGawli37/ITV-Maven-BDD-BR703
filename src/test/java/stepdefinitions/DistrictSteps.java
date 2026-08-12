fd0142e01fd2dfb73fd4b177b7e3d733fa4c2092
Parth Gawli <130574282+ParthGawli37@users.noreply.github.com>
2026-08-12 19:23:31 +0530

    Wire Cucumber steps to real movie booking implementation

--- src/test/java/stepdefinitions/DistrictSteps.java
+++ src/test/java/stepdefinitions/DistrictSteps.java
@@ -1,36 +1,63 @@
 package stepdefinitions;
 
-import io.cucumber.java.en.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pages.HomePage;
+import pages.MovieBookingPage;
 
 public class DistrictSteps {
 
-    HomePage homePage = new HomePage();
+    private final HomePage homePage = new HomePage();
+    private final MovieBookingPage bookingPage = new MovieBookingPage();
 
-    // Background
     @Given("I am on the District.in home page")
     public void open_home_page() {
         homePage.openHomePage();
     }
 
-    @When("I click on the Search icon")
-    public void click_search_icon() {
-        homePage.clickSearchIcon();
+    @When("I open the Movies section")
+    public void open_movies_section() {
+        homePage.openMoviesSection();
     }
 
-    @When("I click on the Movies link")
-    public void click_movies_link() {
-        homePage.clickMoviesLink();
+    @When("I select a current top movie")
+    public void select_current_top_movie() {
+        bookingPage.selectCurrentTopMovie();
     }
 
-    // Scenario steps
-    @When("I enter {string} in the search box")
-    public void enter_movie_name(String movieName) {
-        homePage.enterSearchText(movieName);
+    @When("I select the first theatre with an available showtime")
+    public void select_first_theatre() {
+        bookingPage.selectFirstTheatreWithAvailableShowtime();
     }
 
-    @Then("I should see the search results for {string}")
-    public void verify_search_results(String expectedResult) {
-        homePage.verifySearchResult(expectedResult);
+    @When("I select the first available date")
+    public void select_first_available_date() {
+        bookingPage.selectFirstAvailableDate();
+    }
+
+    @When("I select the earliest available showtime")
+    public void select_earliest_showtime() {
+        bookingPage.selectFirstAvailableShowtime();
+    }
+
+    @Then("at least 2 seats should be available")
+    public void verify_two_seats_available() {
+        bookingPage.assertAtLeastTwoSeatsAvailable();
+    }
+
+    @When("I select the best adjacent seats for 2 people")
+    public void select_best_seats() {
+        bookingPage.selectBestAdjacentSeatsForTwo();
+    }
+
+    @When("I click the Proceed or Check In button")
+    public void click_proceed_or_check_in() {
+        bookingPage.clickProceedOrCheckIn();
+    }
+
+    @Then("the booking flow should continue to the next step")
+    public void verify_booking_flow_continued() {
+        bookingPage.assertBookingFlowContinued();
     }
 }
