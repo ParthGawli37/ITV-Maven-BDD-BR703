412f23b1229cf6397f1f8998dfd53c9f34af76fb
Parth Gawli <130574282+ParthGawli37@users.noreply.github.com>
2026-08-12 19:24:30 +0530

    Allow runtime system-property overrides for CI execution

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -3,21 +3,23 @@ package utils;
 import java.io.FileInputStream;
 import java.util.Properties;
 
-public class ConfigReader {
-	private static Properties prop;
-	
-	static {
-		try {
-			prop = new Properties();
-			FileInputStream fis = new FileInputStream("config.properties");
-			prop.load(fis);
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	public static String get(String key) {
-		return prop.getProperty(key);
-	}
-	
+public final class ConfigReader {
+
+    private static final Properties PROP = new Properties();
+
+    static {
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            PROP.load(fis);
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to load config.properties", e);
+        }
+    }
+
+    private ConfigReader() {
+    }
+
+    public static String get(String key) {
+        String systemValue = System.getProperty(key);
+        return systemValue != null ? systemValue : PROP.getProperty(key);
+    }
 }
